620c1c624dff14d0d0b677ad0ee4de3d3d4531ff
thanhtien <thanhtien@ALIVE-MBPs-MacBook-Pro.local>
2023-05-14 14:25:42 +0700

    StudentManager

---
+++ app/screens/Admin/index.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native/types";
+
+export default function Admin(){
+    return (
+        <View>Admin</View>
+    );
+}
---
+++ app/screens/Teacher/index.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native/types";
+
+export default function Teacher(){
+    return (
+        <View>Teacher</View>
+    );
+}
